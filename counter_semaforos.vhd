35755cd18f9aafebc5eb660174cb2863460c254a
German Lenovo <gn.rant@gmail.com>
2023-04-29 11:54:23 -0300

    max puesto en cero en reset

--- counter_semaforos.vhd
+++ counter_semaforos.vhd
@@ -42,7 +42,7 @@ begin
         --Si reset está activado
         if rst_semf_counter = '1' then
             aux <= (others => '0');
-            max <= '0';
+            max_semf_counter <= '0';
         end if;
         if rising_edge(clk_semf_counter) then
             --Si está habilitado
